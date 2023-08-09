f15ef994f9338066a13ec37e5f6df6f45412c5bb
Madu-f0r0 <duherbert@gmail.com>
2023-08-08 21:36:21 -0700

    Completed task 8 function

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,6 @@
+def uppercase(str):
+    """Prints a string i uppercase"""
+    for i in range(len(str)):
+        print("{:s}".format(chr(ord(str[i]) - 32) if ord(str[i]) >= 97 and
+              ord(str[i]) <= 122 else str[i]), sep="", end="")
+    print()
