b44bc1bcbfe18ccfea06b37a70ffa0dc66abc4c9
Madu-f0r0 <duherbert@gmail.com>
2023-08-09 03:42:08 -0700

    Made modification to add function in task 10

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def add(a, b):
     """adds two integers and returns the result"""
-    if not isinstance(a, int) or not isinstance(b, int):
-        return TypeError
     return a + b
