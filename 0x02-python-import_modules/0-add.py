0a0d21621185542745b3f84e8dfab86d30a99c88
Madu-f0r0 <duherbert@gmail.com>
2023-08-11 02:51:31 -0700

    Completed task 0 program

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from add_0 import add
+    mod = __import__("add_0")
 
+    add = mod.add
     a, b = 1, 2
+
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
