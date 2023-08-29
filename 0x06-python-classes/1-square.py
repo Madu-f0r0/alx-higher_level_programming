4d3f5cf434ebd109d79b49426dadb9fe5d035db8
Madu-f0r0 <duherbert@gmail.com>
2023-08-29 21:42:27 +0100

    Pushed my first OOP

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+class Square:
+    """ Defines the properties of a square.
+
+    To be specific, the square in question is the shape which has
+    all sides of equal length.
+
+    Args:
+        size (int): the length/width of the square
+
+    """
+
+    def __init__(self, size):
+        self.__size = size
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+0x06. Python - Classes and Objects is an introduction to Object Oriented Prgramming (OOP). Here, the concepts of classes and objects in python are introduced and explored
