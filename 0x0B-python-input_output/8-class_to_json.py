6140c6f5aacd87c12a4906440629cebdfa01b9c3
Madu-f0r0 <duherbert@gmail.com>
2023-09-12 20:34:04 +0100

    Completed task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """ This module contains the function class_to_json """
 
-import json
-
 
 def class_to_json(obj):
     """ Returns a dictionary description for JSON seriaization
