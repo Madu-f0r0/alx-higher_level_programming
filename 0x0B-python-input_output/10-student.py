c6868d901d4098d7f70abe3b2476f425458fd13a
Madu-f0r0 <duherbert@gmail.com>
2023-09-14 17:12:54 +0100

    Completed module 10-student version of class Student

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" This module contains the definition of the class Student """
+
+
+class Student:
+    """ Defines the attributes and methods of a Student instance.
+    Attributes:
+        @first_name (str): the first name of the student
+        @last_name (str): the last name of the student
+        @age: the age in years of the student
+    """
+
+    def __init__(self, first_name, last_name,  age):
+        """ Initializes the first name, last name, and age
+        of a Student instance
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Returns the dict representation of a Student instance """
+        if type(attrs) is list:
+            new_dict = {}
+            for attr in attrs:
+                if attr in self.__dict__.keys():
+                    new_dict[attr] = self.__dict__.get(attr)
+            return new_dict
+        return self.__dict__
