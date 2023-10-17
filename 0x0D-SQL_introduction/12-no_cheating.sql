ca33b8c2bdd6d6d6064a9739c385ee616203ff47
Madu-f0r0 <duherbert@gmail.com>
2023-10-17 21:48:43 +0100

    last batch of sql scripts

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,4 @@
+-- This script updates a user's score in table `second_table`
+UPDATE second_table
+SET score = 10
+WHERE name = 'Bob';
--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- This script creates a table `second_table` and fills the attributes`
-CREATE TABLE second_table (
+CREATE TABLE IF NOT EXISTS second_table (
 id INT,
 name VARCHAR(256),
 score INT);
