063f0c533c7e6adde0647340151b6eea35191292
Madu-f0r0 <duherbert@gmail.com>
2023-11-13 22:00:14 +0100

    Added scripts 6 and 7 scripts

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+if (isNaN(process.argv[2])) {
+  console.log('Missing number of occurences');
+} else {
+  const printFrequency = parseInt(process.argv[2]);
+  for (let i = 0; i < printFrequency; i++) {
+    console.log('C is Fun');
+  }
+}
