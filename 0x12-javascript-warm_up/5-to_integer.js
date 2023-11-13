3c2a54cd7874f7628871d04ec7d788be558e132f
Madu-f0r0 <duherbert@gmail.com>
2023-11-13 19:40:52 +0100

    Fixed semistandard errors

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,11 +2,8 @@
 
 const parsedNum = Number.parseInt(process.argv[2]);
 
-if (!parsedNum)
-{
-        console.log("Not a number");
-}
-else
-{
-        console.log("My number: " + parsedNum);
+if (!parsedNum) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + parsedNum);
 }
