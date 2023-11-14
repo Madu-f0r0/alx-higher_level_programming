703737de7c7024875ee6c871b404de55713696f0
Madu-f0r0 <duherbert@gmail.com>
2023-11-14 22:56:53 +0100

    Script 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (!isNaN(w) && !isNaN(h) && w > 1 && h > 1) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let x = 'X';
+      for (let j = 1; j < this.width; j++) { /* Can also use `repeat()` method */
+        x += 'X';
+      }
+      console.log(x);
+    }
+  }
+
+  rotate () {
+    let temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
