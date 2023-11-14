bba8f3eca9e5ce551d8263994f9e2957ee9e802d
Madu-f0r0 <duherbert@gmail.com>
2023-11-14 09:25:28 +0100

    Final refactoring for script 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const printFrequency = parseInt(process.argv[2]);
-if (isNaN(printFrequency)) {
+if (!printFrequency) {
   console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < printFrequency; i++) {
-    console.log('C is Fun');
+    console.log('C is fun');
   }
 }
