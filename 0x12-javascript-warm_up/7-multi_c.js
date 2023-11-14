956aa82a7f9c43572bae4ae46f2508def18f3b8d
Madu-f0r0 <duherbert@gmail.com>
2023-11-14 08:43:48 +0100

    Refactored script 7 code

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const printFrequency = parseInt(process.argv[2]);
-if (!printFrequency) {
+if (isNaN(printFrequency)) {
   console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < printFrequency; i++) {
