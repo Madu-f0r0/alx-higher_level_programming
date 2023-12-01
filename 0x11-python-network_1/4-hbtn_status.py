51de8f5512c98e174fd0fb15f678962c674bae7b
Madu-f0r0 <duherbert@gmail.com>
2023-12-01 17:51:30 +0100

    Script 4 modified

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,6 +5,6 @@ if __name__ == '__main__':
     import requests
 
     r = requests.get('https://alx-intranet.hbtn.io/status')
-    print('Body response')
+    print('Body response:')
     print('\t- type: {}'.format(r.text.__class__))
     print('\t- content: {}'.format(r.text))
