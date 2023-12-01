bb5f7b09585793907a3e2722e94584d928496f84
Madu-f0r0 <duherbert@gmail.com>
2023-12-01 18:31:40 +0100

    Script 5 modified

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -3,10 +3,10 @@
 of the variable X-Request-Id in the response header
 """
 
-import requests
-import sys
-
 if __name__ == '__main__':
+
+    import requests
+    import sys
 
     url = sys.argv[1]
     r = requests.get(url)
