2d4560e1b9df3331329ceda7d4760d5600d3c060
Madu-f0r0 <duherbert@gmail.com>
2024-01-03 19:13:03 +0100

    Webscraping 3

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const request = require('request');
+const apiURL = process.argv[2];
+const idURL = 'https://swapi-api.alx-tools.com/api/people/18/';
+
+request(apiURL, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const allMovies = JSON.parse(body).results;
+    let idMovies = 0;
+
+    allMovies.forEach(movie => {
+      if (movie.characters.includes(idURL)) {
+        idMovies++;
+      }
+    });
+
+    console.log(idMovies);
+  }
+});
