383e85aae79ac24232f5e9e7002af95340718154
Madu-f0r0 <duherbert@gmail.com>
2024-01-03 17:49:05 +0100

    Webscraping 2

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,6 +5,9 @@ const movieId = process.argv[2];
 const starwarsURL = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
 request(starwarsURL, (err, response, body) => {
-  // const bodyJSON = JSON.parse(body)
-  console.log(JSON.parse(body).title);
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
 });
